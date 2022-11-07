4c2e68399ace538a682f5a5cedc6d6a82896e709
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2022-11-07 08:18:20 -0500

    Improved toString() of map and biome generation

--- src/main/java/Game/Map/Map.java
+++ src/main/java/Game/Map/Map.java
@@ -7,26 +7,27 @@ import java.util.Queue;
 import java.util.Random;
 
 public class Map {
-    private Square[][] map;
-    private Random r = new Random();
+    private final Square<?>[][] map;
+    private final Random r = new Random();
     public Map(){
         map = new Square[16][16];
         generateNewMap();
     }
-    //TODO: figure out to generate new list
+    //TODO: Better Space Out Biomes && Randomize Biome Chosen
     public void generateNewMap(){
         int[][] biomeArr = new int[16][16];
         boolean[][] colored = new boolean[16][16];
         Queue<Pair> q = new LinkedList();
-        int plates = generateRandomNumber(4,6);
-        final String[] biomes = chooseBiomes(plates);
+        int plates = generateRandomNumber(5,6);
+        final Square[] biomes = {LandBiome.FOREST, LandBiome.MOUNTAIN, LandBiome.DESERT, LandBiome.GRASSLANDS, LandBiome.TUNDRA, WaterBiome.LAKE, LandBiome.RAINFOREST, WaterBiome.OCEAN};
         for(int i = 1; i < plates+1; i++){
-            int x = generateRandomNumber(3,12);
-            int y = generateRandomNumber(3,12);
+            int x = generateRandomNumber(5,11);
+            int y = generateRandomNumber(5,11);
             while(colored[x][y]){
-                x = generateRandomNumber(3,12);
-                y = generateRandomNumber(3,12);
+                x = generateRandomNumber(5,11);
+                y = generateRandomNumber(5,11);
             }//TODO: need to figure out how to assign the biomes
+            map[x][y] = biomes[i];
             biomeArr[x][y] = i;
             colored[x][y] = true;
             addBorders(q, colored, x, y);
@@ -36,17 +37,21 @@ public class Map {
             if(colored[temp.x][temp.y]){
                 continue;
             }
-            assign(colored,biomeArr,temp.x,temp.y);
+            assign(colored,biomeArr,temp.x,temp.y, biomes);
             addBorders(q,colored,temp.x,temp.y);
         }
-        for(int i = 0; i < 16; i++){
-            System.out.println();
+        ensure(colored,biomeArr,biomes);
+    }
+    private void ensure(boolean[][] colored, int[][] biomeArr, Square[] biomes){
+        for(int i = 0; i < 16;i++){
             for(int j = 0; j < 16; j++){
-                System.out.print(biomeArr[i][j] + " ");
+                if(map[i][j] == null){
+                    assign(colored, biomeArr,i,j, biomes);
+                }
             }
         }
     }
-    private void assign(boolean[][] colored, int[][] biomeArr, int x, int y){
+    private void assign(boolean[][] colored, int[][] biomeArr, int x, int y, Square[] biomes){
         int choice = generateRandomNumber(0, 3);
         boolean changed = false;
         while(!changed){
@@ -55,6 +60,7 @@ public class Map {
                     if(x+1 < 16 && colored[x+1][y]) {
                         colored[x][y] = true;
                         biomeArr[x][y] = biomeArr[x + 1][y];
+                        this.map[x][y] = biomes[biomeArr[x + 1][y]];
                     }
                     changed = true;
                     break;
@@ -62,6 +68,7 @@ public class Map {
                     if(x-1 >= 0 && colored[x-1][y]){
                         colored[x][y] = true;
                         biomeArr[x][y] = biomeArr[x-1][y];
+                        this.map[x][y] = biomes[biomeArr[x - 1][y]];
                     }
                     changed = true;
                     break;
@@ -69,6 +76,7 @@ public class Map {
                     if(y+1 < 16 && colored[x][y+1]){
                         colored[x][y] = true;
                         biomeArr[x][y] = biomeArr[x][y+1];
+                        this.map[x][y] = biomes[biomeArr[x][y+1]];
                     }
                     changed = true;
                     break;
@@ -76,6 +84,7 @@ public class Map {
                     if(y-1 >= 0 && colored[x][y-1]){
                         colored[x][y] = true;
                         biomeArr[x][y] = biomeArr[x][y-1];
+                        this.map[x][y] = biomes[biomeArr[x][y-1]];
                     }
                     changed = true;
                     break;
@@ -95,6 +104,18 @@ public class Map {
         if(y-1 >= 0 && !colored[x][y-1]){
             q.add(new Pair(x,y-1));
         }
+        if(x+1 < 16 && y+1 < 16 && !colored[x+1][y+1]){
+            q.add(new Pair(x+1,y+1));
+        }
+        if(x+1 < 16 && y-1 >= 0 && !colored[x+1][y-1]){
+            q.add(new Pair(x+1,y-1));
+        }
+        if(y+1 < 16 && x-1 >= 0 && !colored[x-1][y+1]){
+            q.add(new Pair(x-1,y+1));
+        }
+        if(y-1 >= 0 && x-1 >= 0 && !colored[x-1][y-1]){
+            q.add(new Pair(x-1,y-1));
+        }
     }
     private class Pair{
         int x;
@@ -103,20 +124,6 @@ public class Map {
             this.x = x;
             this.y = y;
         }
-    }
-    private String[] chooseBiomes(int count){
-        String[] biomes = new String[count];
-        final String[] selection = {"FOREST","MOUNTAIN", "DESERT", "GRASSLANDS", "TUNDRA", "RAINFOREST","LAKE","OCEAN"};
-        boolean[] selected = new boolean[8];
-        for(int i = 0; i < count; i++){
-            int choice = generateRandomNumber(0,7);
-            while(selected[choice]){
-                choice = generateRandomNumber(0,7);
-            }
-            selected[choice] = true;
-            biomes[i] = selection[choice];
-        }
-        return biomes;
     }
     private int generateRandomNumber(int lowerBound, int upperBound){
         return r.nextInt((upperBound-lowerBound)+1)+lowerBound;
@@ -129,14 +136,14 @@ public class Map {
        return "";
     }
     public void printGameStatus(){
-        for(int i = 0; i < 17; i++){
+        for(int i = 0; i < 16; i++){
             System.out.println("");
             if(i < 7){
-                System.out.print(Color.RESET + "" + Color. WHITE_BACKGROUND + "" + Color.RED_BOLD_BRIGHT+ " " + Math.abs(15-i) + " ");
+                System.out.print(Color.RESET + "" + Color. WHITE_BACKGROUND + "" + Color.RED_BOLD_BRIGHT+ " " + Math.abs(16-i) + " ");
             }else{
-                System.out.print(Color.RESET + "" + Color. WHITE_BACKGROUND + "" + Color.RED_BOLD_BRIGHT + " " + Math.abs(15-i) + "  ");
+                System.out.print(Color.RESET + "" + Color. WHITE_BACKGROUND + "" + Color.RED_BOLD_BRIGHT + " " + Math.abs(16-i) + "  ");
             }
-            for(int j = 15; j >= 0; j--){
+            for(int j = 15; j >= 0 && i < 16; j--){
                 switch (map[i][j].getSquare()) {
                     case "FOREST" -> System.out.print(Color.RESET + "" + Color.BLACK_BACKGROUND + ""+  Color.GREEN_BOLD +" " + "F" + "  " + Color.RESET);
                     case "MOUNTAIN" -> System.out.print(Color.RESET + "" + Color.BLACK_BACKGROUND + ""+ Color.WHITE_BOLD_BRIGHT +" " + "M" + "  "+ Color.RESET);
@@ -150,7 +157,7 @@ public class Map {
             }
         }
         System.out.println();
-        System.out.print(Color. WHITE_BACKGROUND + "    " + Color.RESET);
+        System.out.print(Color. WHITE_BACKGROUND + "" + Color.RED_BOLD_BRIGHT+ " 0  " + Color.RESET);
         for(int i = 1; i <= 16; i++){
             if(i < 10){
                 System.out.print(Color. WHITE_BACKGROUND+ "" + Color.RED_BOLD_BRIGHT + " " + i + "  ");
